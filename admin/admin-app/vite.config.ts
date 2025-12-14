6ff25ce67c319b033b85f8fecd0e88d4ba8fc9e6
Kartik Sharma <sharma.kartik180@gmail.com>
2025-12-14 15:23:12 +0530

    fix: add server configuration for allowed hosts in Vite config

--- admin/admin-app/vite.config.ts
+++ admin/admin-app/vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      'laya-server-m-env.eba-qw4gep8b.ap-south-1.elasticbeanstalk.com'
+    ]
+  }
 })
